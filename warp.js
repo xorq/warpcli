2df754f3e464c4a53159b9d8275ae017df7af7fa
xorq <xorque@gmail.com>
2016-09-15 14:28:47 +0700

    less memory required

--- warp.js
+++ warp.js
@@ -1,7 +1,7 @@
 // THE SCRYPT MODULE IS LOADED FROM https://github.com/tonyg/js-scrypt/blob/master/browser/scrypt.js
 
 var scrypt_module_factory = (function (requested_total_memory) {
-    var Module = {TOTAL_MEMORY: (requested_total_memory || 32554432)};
+    var Module = {TOTAL_MEMORY: (requested_total_memory || 10554432)};
     var scrypt_raw = Module;
 function g(a) {
   throw a;
